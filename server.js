d44eab9db7837f4e1ce438c3149c3af0dfbf3f8d
Kitch41 <stefkeuken@hotmail.com>
2023-02-20 17:21:34 +0100

    Fixed server.js error

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-$null 
+
+const log = console.log;
+
+const express = require('express');
+const PORT = process.env.PORT || 1337;
+const app = express();
+
+
+//Home Get
+
+app.get('/', onHome).listen(PORT);
+
+function onHome(req, res) {
+    res.send('hallo')
+};
+
+//User Get
+
+app.get('/user', onUser).listen(PORT);
+
+function onUser(req, res) {
+    res.send('hallo user')
+};
+
+
+app.listen(PORT);
