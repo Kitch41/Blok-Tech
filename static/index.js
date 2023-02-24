6628a7fb5b2f6c398e89524034301c4e69165ce6
Kitch41 <stefkeuken@hotmail.com>
2023-02-24 11:29:29 +0100

    start static page

--- static/index.js
+++ static/index.js
@@ -1,11 +1,48 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const home = document.querySelector('.home');
+const explore = document.querySelector('.explore');
+const myGames = document.querySelector('.mygames');
+const myBuddys = document.querySelector('.mybuddys');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const activeyn = 0;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+function activateexplore(){
+  if (activeyn == 0){
+    explore.classList.add("active");
+    myGames.classList.remove("active");
+    myBuddys.classList.remove("active");  
+    activeyn = 1;
+  }
+  else {
+    explore.classList.remove("active");
+      activeyn = 0;
+  }
+}
+function activatemyGames(){
+  if (activeyn == 0){
+    myGames.classList.add("active");
+    explore.classList.remove("active");
+    myBuddys.classList.remove("active");  
+    activeyn = 1;
+  }
+  else {
+      myGames.classList.remove("active");
+    activeyn = 0;
+  }
+}
+function activatemyBuddys(){
+  if (activeyn == 0){
+    myBuddys.classList.add("active");
+    explore.classList.remove("active");
+    myGames.classList.remove("active");  
+    activeyn = 1;
+  }
+  else {
+    myBuddys.classList.remove("active");
+    activeyn = 0;
+  }
+}
+
+
+explore.addEventListener("click", activateexplore);
+myGames.addEventListener("click", activatemyGames);
+myBuddys.addEventListener("click", activatemyBuddys);
