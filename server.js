b4ae52ddb47910ef3f783ab84a9c03ae4f0479af
Stef Keuken <91873549+Kitch41@users.noreply.github.com>
2023-03-07 13:35:02 +0100

    compact server.js

--- server.js
+++ server.js
@@ -8,40 +8,41 @@ app.set('view engine', 'ejs');
 
 //Home Get
 
-app.get('/', onHome)
-
-function onHome(req, res) {
+app.get('/', (req, res) => {
   res.render('index.ejs');
-};
+})
+
 
 //User Get
 
-app.get('/user', onUser)
-
-function onUser(req, res) {
-    res.send('hallo user')
-};
+app.get('/user', (req, res) => {
+  res.send('hallo user')
+})
 
 //profile get
 
-app.get('/user', onProfile)
+app.get('/user', (req, res) => {
+  res.send('hallo profile')
+})
 
-function onProfile(req, res) {
-    res.send('hallo profile')
-};
-
-app.get('/user', onProfile)
-
-function onProfile(req, res) {
-    res.send('hallo profile')
-};
-
-app.get('*', onError)
-
-function onError(req, res) {
+app.get('*', (req, res) => {
   res.send("error 404, page not found")
-}
+})
 
+
+app.get('/edit', (req, res) => {
+  res.render('edit.ejs')
+})
+
+
+// submitting new info
+
+app.post('/edit', (req,res) => {
+    res.render
+})
+
+
+// listener
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
